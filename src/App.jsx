43c44403f159781b2bad7ed5e426c599f5ea10a9
Elisabeth Røysland <elisabeth.skartveit@lyse.net>
2025-09-04 09:43:26 +0200

    Created Dashboard component and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
     return (
-        <p>Hello, world!</p>
+        <main>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+            </Routes>
+        </main>
     );
 }
 
