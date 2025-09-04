fd3a4768e6c83d20fb369796e33e314e331890b1
Elisabeth Røysland <elisabeth.skartveit@lyse.net>
2025-09-04 10:04:23 +0200

    Set up data fetching from the contact api and display a list of all contact in dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect, createContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Dashboard from './components/Dashboard';
 import './App.css';
 
+export const ContactContext = createContext();
+
 function App() {
+
+    const [contacts, setContacts] = useState([]);
+
+    const contact_api = "https://boolean-uk-api-server.fly.dev/elisabethroys/contact"
+
+    useEffect(() =>
+    {
+        const fetchContacts = async () => {
+            const response = await fetch(contact_api);
+            const data = await response.json();
+            setContacts(data);
+        }
+
+        fetchContacts();
+    }, []);
+
     return (
         <main>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-            </Routes>
+            <ContactContext.Provider value={{contacts}}>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                </Routes>
+            </ContactContext.Provider>
         </main>
     );
 }
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,6 +1,25 @@
+import { useContext } from 'react'
+import { ContactContext } from "../../App"
+
 function Dashboard () {
+
+    const { contacts } = useContext(ContactContext);
+
     return (
-        <h1>Dashboard</h1>
+        <>
+            <h1>Dashboard</h1>
+            <h2>Contacts</h2>
+            <ul>
+                {contacts.map((contact, index) => (
+                    <li key={index}>
+                        <h3>
+                            {contact.firstName} {contact.lastName}
+                        </h3>
+                    </li>
+                        
+                ))}
+            </ul>
+        </>
     )
 }
 
